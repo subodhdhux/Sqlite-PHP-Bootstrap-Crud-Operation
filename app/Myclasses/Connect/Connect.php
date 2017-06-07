5131d3f109346db44a2b0622b9887ff6bf076994
developersubodh10@gmail.com <developersubodh10@gmail.com>
2017-06-07 16:36:09 +0545

    asd

--- app/Myclasses/Connect/Connect.php
+++ app/Myclasses/Connect/Connect.php
@@ -8,30 +8,22 @@ class Connect
 
     protected function __construct()
     {
-
-
          //$this->db_name = 'c:\sqlite\db\project.db';
          $this->db_name = 'project.db';
         
         if ($this->db === null) 
         {
-
-            echo "Again here";
         	if (realpath($this->db_name)) {
-                echo "Again here1";
         		$this->db = new \SQLite3($this->db_name);
+        		
 			}
 			else
-			{    echo "Again here11";
-				//$db = fopen($this->db_name, "w");
+			{
+				$db = fopen($this->db_name, "w");
 				$this->db = new \SQLite3($this->db_name);
-				//fclose($db);
+				fclose($db);
 			}
-
-            $this->db = new \SQLite3($this->db_name);
         }
-
-         die("I am here");
     }
 
     protected function __clone()
